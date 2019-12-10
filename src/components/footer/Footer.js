f08efee2b53faaffce57fcabcca27edd5d3ccd5e
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-10 14:08:08 -0600

    added tmbd logo as svg file in images folder. App now links to saved picture instead of external link.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,12 +9,12 @@ class Footer extends Component {
                 <div className="footer-content">
                     <div className="tmbd-credit">
                         <picture>
-                            <img id="tmbd-logo" src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt="The Movie Database" />
+                            <img id="tmbd-logo" src={require("../../images/tmbd-logo.svg")} alt="The Movie Database" />
                         </picture>
                         <div className="tmbd-text">
                             <p>This product uses the TMDb API but is not endorsed or certified by TMDb.
                     </p>
-                            <a href="https://www.themoviedb.org/" target="_blank">www.themoviedb.org</a>
+                            <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">www.themoviedb.org</a>
                         </div>
                     </div>
                 </div>

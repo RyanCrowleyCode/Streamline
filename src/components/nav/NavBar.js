7caab3e0cf607fb9ddf32091605728d8aff6870e
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-10 10:39:56 -0600

    Links are visible on NavBar, and so is button for username. Links route to the desired view.

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,12 +1,39 @@
+/*
+    NavBar.js
+
+    Purpose: This component is responsible for rendering the NavBar.
+
+    Author(s): Ryan Crowley
+*/
+
+// REACT
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+// Styles
 import './NavBar.css'
 
 class NavBar extends Component {
     render () {
         return (
-            <React.Fragment>
-                <h1>NavBar!</h1>
-            </React.Fragment>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Movies</Link>
+                    </li>
+                    <li>
+                        <Link to="/watchlists">Watchlists</Link>
+                    </li>
+                    <li>
+                        <button>USERNAME</button>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }

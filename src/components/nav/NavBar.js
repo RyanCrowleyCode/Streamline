60409e4ab7f8dffca22cbcc0f72736568803940c
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-10 11:03:04 -0600

    experimenting with react-bootstrap

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -11,27 +11,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 // Styles
 // import './NavBar.css'
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 
 class NavBar extends Component {
     render () {
+        const btnLink = {
+            border: "none",
+            background: "none",
+            color: "white"
+        }
         return (
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Movies</Link>
+            <nav className="navbar bg-dark text-white flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Movies</Link>
                     </li>
-                    <li>
-                        <Link to="/watchlists">Watchlists</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/watchlists">Watchlists</Link>
                     </li>
-                    <li>
-                        <button>USERNAME</button>
+                    <li className="nav-item">
+                        <button className="nav-link" style={btnLink}>USERNAME</button>
                     </li>
-                    <li>
-                        <Link to="/login">Login</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
                     </li>
-                    <li>
-                        <Link to="/register">Register</Link>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Register</Link>
                     </li>
                 </ul>
             </nav>

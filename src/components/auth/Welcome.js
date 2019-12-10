755c7e8429c7075400570a398007fe7f75fe8074
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-10 14:35:02 -0600

    added Welcome.js which will have welcome message and render Registration and Login

---
+++ src/components/auth/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+
+    render () {
+        return (
+            <React.Fragment>
+                <h1>Welcome!</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Weclome

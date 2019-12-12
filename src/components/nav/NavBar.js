1f44322040c65fb67374f1dc3ae8a4e53bed7cae
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-12 09:35:55 -0600

    Navbar receives isLoggedIn as props. Navbar dynamically renders links to views based on isLoggedIn

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -13,34 +13,39 @@ import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 class NavBar extends Component {
-    render () {
+    render() {
         // styles for button link
         const btnLink = {
             border: "none",
             background: "none",
             color: "white"
         }
-        
+
         return (
             <nav className="navbar bg-dark text-white flex-md-nowrap p-0 shadow">
-                <h3>Streamline</h3>
-                <ul className="nav nav-pills nav-fill">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Movies</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/watchlists">Watchlists</Link>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" style={btnLink}>USERNAME</button>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Register</Link>
-                    </li>
-                </ul>
+                <h3>STREAMLINE</h3>
+                {this.props.isLoggedIn ?
+                    <ul className="nav nav-pills nav-fill">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Movies</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/watchlists">Watchlists</Link>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link" style={btnLink}>USERNAME</button>
+                        </li>
+                    </ul>
+                    :
+                    <ul className="nav nav-pills nav-fill">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </ul>
+                }
             </nav>
         )
     }

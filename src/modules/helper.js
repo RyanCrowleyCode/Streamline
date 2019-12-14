614601a607622be39e74ef67fb206996414c14bd
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-14 11:40:04 -0600

    working on helper function to convert date strings

---
+++ src/modules/helper.js
@@ -0,0 +1,18 @@
+/*
+    helper.js
+
+    Purpose:    This module exports helper functions that can be imported into
+                components. 
+
+    Author(s): Ryan Crowley
+*/
+
+// Converts a date in the form of 'YYYY-MM-DD' to 'Month Day, Year'
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+export function toDatePhrase(date) {
+    const month = date.slice(5, 7)
+    const year = date.slice(0, 4)
+    const day = date.slice(8, 10)
+}

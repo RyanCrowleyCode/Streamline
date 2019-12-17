92bfefc8efff756a7dd8fe2e39448b4cb81a67ca
Ryan Crowley <ryancrowleycode@gmail.com>
2019-12-17 16:01:29 -0600

    moved WatchlistForm inside of WatchlistList. Working on getting the modal to close when submitting.

--- src/components/watchlists/WatchlistList.js
+++ src/components/watchlists/WatchlistList.js
@@ -11,6 +11,7 @@
 import React, { Component } from 'react'
 import WatchlistForm from './WatchlistForm'
 import Popup from "reactjs-popup";
+import { Form, Button } from 'react-bootstrap'
 
 // STYLES
 import './WatchlistList.css'
@@ -26,7 +27,60 @@ import { getLoggedInUser } from '../../modules/helper'
 
 class Watchlists extends Component {
     state = {
-        watchlists: []
+        watchlists: [],
+        listName: '',
+        listDescription: '',
+        loadingStatus: false
+    }
+
+    // update listName and Description in state with every keystroke in input field
+    handleFieldChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    // handle adding new watchlist
+    handleSubmit = e => {
+        e.preventDefault()
+        const { listName, listDescription } = this.state
+
+        // make sure fields are filled out
+        if (listName && listDescription) {
+            this.setState({ loadingStatus: true })
+
+            const newWatchlist = {
+                listName: this.state.listName,
+                listDescription: this.state.listDescription,
+                userId: getLoggedInUser()
+            }
+
+            // get watchlists for user. If no watchlist with the same name
+            // exists, create new watchlist. Otherwise, let the user know
+            // that they already have a watchlist with this name.
+            watchlistApiManager.getOwnWatchlists(getLoggedInUser())
+                .then(watchlists => {
+                    let isNew = true
+                    watchlists.map(list => {
+                        if (newWatchlist.listName === list.listName) {
+                            isNew = false
+                        }
+                    })
+                    return isNew
+                })
+                .then(isNew => {
+                    if (isNew) {
+                        // post newWatchlist to the database and close modal
+
+                    } else {
+                        window.alert("You already have a Watchlist with that name.")
+                        this.setState({ loadingStatus: false })
+                    }
+                })
+
+
+        } else {
+            window.alert("Please fill out all of the fields.")
+            this.setState({ loadingStatus: false })
+        }
     }
 
     componentDidMount() {
@@ -47,7 +101,32 @@ class Watchlists extends Component {
                         </button>}
                     modal
                     closeOnDocumentClick>
-                    <WatchlistForm close={this.close}/>
+                    <Form>
+                        <h4>New Watchlist</h4>
+                        <Form.Group>
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                id="listName"
+                                type="text"
+                                placeholder="Funny Movies"
+                                onChange={this.handleFieldChange} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                id="listDescription"
+                                type="text"
+                                placeholder="Write something to describe your list."
+                                onChange={this.handleFieldChange} />
+                        </Form.Group>
+                        <Button
+                            variant="success"
+                            type="button"
+                            onClick={this.handleSubmit}
+                            disabled={this.state.loadingStatus}>
+                            Submit
+                </Button>
+                    </Form>
                 </Popup>
                 <section className="watchlist-list">
                     {this.state.watchlists.map(watchlist =>
